15f2ba52fd50123156a466bcee25dc6511f4642d
yousef_fj <yousef.fawzy.2000@gmail.com>
2020-07-17 09:58:53 +0300

    Add departments page and some layout changes

---
+++ SimpleHospitalSystem/Pages/Departments.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SimpleHospitalModel.DBModel;
+using SimpleHospitalModel.HospitalRepository;
+
+namespace SimpleHospitalSystem.Pages
+{
+    public class DepartmentsModel : PageModel
+    {
+        private readonly IHospitalRepository repository;
+
+        public DepartmentsModel(IHospitalRepository repository)
+        {
+            this.repository = repository;
+        }
+        public IList<Department> Departments { get; set; } 
+        public async Task OnGetAsync()
+        {
+            Departments = await repository.GetDepartmentsWithBedAsync();
+        }
+    }
+}
